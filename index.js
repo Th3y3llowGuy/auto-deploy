ceafa4054d8a39115ec2fac042ac1519e651fa01
th3y3llowguy <theyellowguycodes@gmail.com>
2023-09-24 03:19:25 +0200

    v4

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => res.json({ message: "OK v3" }));
+app.get("/", (req, res) => res.json({ message: "OK v4" }));
 
 app.listen(3000);
