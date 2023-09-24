03d477769af5609ae2a352750ee0e81b84e4908b
th3y3llowguy <theyellowguycodes@gmail.com>
2023-09-24 03:17:47 +0200

    v3

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => res.json({ message: "OK v2" }));
+app.get("/", (req, res) => res.json({ message: "OK v3" }));
 
 app.listen(3000);
